4f7a94bb629cbd9e3d34e0a7e2fad546f80c2b40
JP Lynch <JPLynch35@gmail.com>
2018-07-14 09:37:17 -0600

    adds database tables for all models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180714151346) do
+ActiveRecord::Schema.define(version: 20180714153615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "gif_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["gif_id"], name: "index_favorites_on_gif_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "gifs", force: :cascade do |t|
+    t.string "gif_url"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_gifs_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "favorites", "gifs"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "gifs", "categories"
 end
