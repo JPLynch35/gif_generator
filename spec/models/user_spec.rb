bf48855c2933a2c249539e169dc2512a30199c98
JP Lynch <JPLynch35@gmail.com>
2018-07-15 21:39:46 -0600

    adds authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,17 @@ describe User, type: :model do
     it { should have_many(:favorites)}
     it { should have_many(:gifs).through(:favorites)}
   end
+  describe 'class methods' do
+    it 'should authenticate a valid user' do
+      user = User.create(name: 'Dennis', email: 'Dennis@gmail.com', password: 'thisismypassword')
+
+      attempt1 = user.authenticate(user.email, 'thisisnotmypassword')
+
+      expect(attempt1).to eq(nil)
+
+      attempt2 = user.authenticate(user.email, 'thisismypassword')
+
+      expect(attempt2).to eq(user)
+    end
+  end
 end
