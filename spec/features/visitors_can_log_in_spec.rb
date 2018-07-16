5a14b996a47c24b1f4549d3c43e1f289b6c17311
JP Lynch <JPLynch35@gmail.com>
2018-07-15 20:51:18 -0600

    adds feature tests for creating account and log in

--- spec/features/visitors_can_log_in_spec.rb
+++ spec/features/visitors_can_log_in_spec.rb
@@ -3,7 +3,25 @@ require 'rails_helper'
 describe 'a visitor' do
   describe 'visiting /' do
     it 'can create an account' do
-      
+      visit root_path
+      click_on 'Create Account'
+
+      expect(current_path).to eq(new_user_path)
+
+      fill_in user[name], with: 'Paul'
+      fill_in user[email], with: 'Paul@gmail.com'
+      fill_in user[password], with: 'totallysecret'
+      click_on 'Create'
+
+      expect(current_path).to eq(user_path(User.where(email: 'Paul@gmail.com')))
+    end
+    it 'can create log in' do
+      user1 = User.create(name: 'Dennis', email: 'Dennis@gmail.com', password: 'thisismypassword')
+      fill_in user[email], with: user1.email
+      fill_in user[password], with: user1.password
+      click_on 'Log In'
+
+      expect(current_path).to eq(user_path(User.where(email: 'Dennis@gmail.com')))
     end
   end
 end
