e12e45dbcf11db81ef4fd66c207b90733ebf9eaa
JP Lynch <JPLynch35@gmail.com>
2018-07-16 07:27:38 -0600

    adds feature tests for user favorites

--- spec/features/user_can_see_a_list_of_all_favorite_gifs_spec.rb
+++ spec/features/user_can_see_a_list_of_all_favorite_gifs_spec.rb
@@ -26,4 +26,29 @@ describe 'a logged-in user' do
       expect(page).to_not have_content(@gif4.gif_url)
     end
   end
+  describe 'visiting /gifs' do
+    before :each do
+      @user1 = User.create(name: 'Bill', email: 'Bill@Bill.Bill', password: 'Bob')
+      @category1 = Category.create(title: 'dogs')
+      @gif1 = @category1.gifs.create(gif_url: 'www.testurl.com/test1')
+      @gif2 = @category1.gifs.create(gif_url: 'www.testurl.com/test2')
+      @category2 = Category.create(title: 'cats')
+      @gif3 = @category2.gifs.create(gif_url: 'www.testurl.com/test3')
+      @gif4 = @category2.gifs.create(gif_url: 'www.testurl.com/test4')
+    end
+    it 'can select favorite gifs from gifs index page' do
+      visit gifs_path
+      check "gif_#{@gif1.id}"
+      check "gif_#{@gif4.id}"
+      visit user_path(@user1)
+      click_on 'Favorites'
+
+      expect(page).to have_content(@category1.title)
+      expect(page).to have_content(@gif1.gif_url)
+      expect(page).to_not have_content(@gif2.gif_url)
+      expect(page).to have_content(@category2.title)
+      expect(page).to_not have_content(@gif3.gif_url)
+      expect(page).to have_content(@gif4.gif_url)
+    end
+  end
 end
