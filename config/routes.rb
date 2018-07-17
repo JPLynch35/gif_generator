4d272d75be1f92725fb7afe58d8887637db2d897
JP Lynch <JPLynch35@gmail.com>
2018-07-17 13:49:43 -0600

    adds admin path for categories and feature tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :admin do
+    resources :categories, only: [:index, :new, :create, :destory]
+  end
   resources :gifs, only: [:index]
   resources :users, except: [:destroy, :edit, :update] do
     resources :favorites, only: [:index, :create, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20180717191354) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "role"
+    t.integer "role", default: 0
   end
 
   add_foreign_key "favorites", "gifs"
