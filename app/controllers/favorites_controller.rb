3f3da250cd15527c248b50e727cc2c02866a1d43
JP Lynch <JPLynch35@gmail.com>
2018-07-17 12:21:43 -0600

    fixes favorites index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
 class FavoritesController < ApplicationController
   def index
-    @favorites = Favorite.where(user_id: current_user).includes(:gif)
-    @user = current_user
+    @user = User.find(params[:user_id])
+    @favorites = Favorite.where(user_id: @user).includes(:gif)
   end
 
   def create
-    Favorite.create_favorites(params[:favorites], current_user)
-    redirect_to user_favorites_path(current_user)
+    @user = User.find(params[:user_id])
+    Favorite.create_favorites(params[:favorites], @user)
+    redirect_to user_favorites_path(@user)
   end
 
   def destroy
+    @user = User.find(params[:user_id])
     Favorite.destroy(params[:id])
-    redirect_to user_favorites_path(current_user)
+    redirect_to user_favorites_path(@user)
   end
 end
