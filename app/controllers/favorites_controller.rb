e9a0193a44c3a1e9e81dbec64f6dd725bbb2bce6
JP Lynch <JPLynch35@gmail.com>
2018-07-17 08:42:47 -0600

    replaced @user with current_user in user show and favorites index

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
   def index
-    @favorites = Favorite.where(user_id: session[:user_id]).includes(:gif)
-    @user = User.find(session[:user_id])
+    @favorites = Favorite.where(user_id: current_user).includes(:gif)
+    @user = current_user
   end
 
   def create
-    Favorite.create_favorites(params[:favorites], session[:user_id])
-    redirect_to user_favorites_path(session[:user_id])
+    Favorite.create_favorites(params[:favorites], current_user)
+    redirect_to user_favorites_path(current_user)
   end
 
   def destroy
     Favorite.destroy(params[:id])
-    redirect_to user_favorites_path(session[:user_id])
+    redirect_to user_favorites_path(current_user)
   end
 end
