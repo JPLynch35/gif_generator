1a23c7ecd5fe07c8b89475fc82c1c1005cb1a2d3
JP Lynch <JPLynch35@gmail.com>
2018-07-17 13:25:55 -0600

    adds roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :password, :name, require: true
+  validates_presence_of :password, :name, :role, require: true
   has_secure_password
   has_many :favorites
   has_many :gifs, through: :favorites
-
+  enum role: ['default', 'admin']
 end
