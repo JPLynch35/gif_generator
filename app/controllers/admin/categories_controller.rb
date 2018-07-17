353a2188104a7db8f9f1f1586814d52de5a9318e
JP Lynch <JPLynch35@gmail.com>
2018-07-17 17:18:32 -0600

    adds safeguard against failed searches

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,30 +10,31 @@ class Admin::CategoriesController < Admin::BaseController
   def create
     api_instance = GiphyClient::DefaultApi.new
     api_key = "6KjjikGzCZdEMgXtaEt9JUyg7nCjIDY0"
-    q = params[:title] # String | Search query term or prhase.
     opts = { 
-      limit: 1, # Integer | The maximum number of records to return.
-      offset: 0, # Integer | An optional results offset. Defaults to 0.
+      tag: params[:title], # String | Filters results by specified tag.
       rating: "g", # String | Filters results by specified rating.
-      lang: "en", # String | Specify default country for regional content; use a 2-letter ISO 639-1 country code. See list of supported languages <a href = \"../language-support\">here</a>.
       fmt: "json" # String | Used to indicate the expected response format. Default is Json.
     }
     begin
       #Search Endpoint
-      result = api_instance.gifs_search_get(api_key, q, opts).data.first.images.fixed_height.url
+      result = api_instance.gifs_random_get(api_key, opts).data
     rescue GiphyClient::ApiError => e
-      puts "Exception when calling DefaultApi->gifs_search_get: #{e}"
+      result = "Error"
     end
+    
+    unless result.nil?
+      if Category.find_by_title(params[:title])
+        category_id = Category.find_by_title(params[:title]).id
+      else
+        Category.create(title: params[:title])
+        category_id = Category.last.id
+      end
 
-    if Category.find_by_title(params[:title])
-      category_id = Category.find_by_title(params[:title]).id
+      Gif.create(gif_url: result.image_url, category_id: category_id )
+      redirect_to admin_categories_path
     else
-      Category.create(title: params[:title])
-      category_id = Category.last.id
+      render :new
     end
-
-    Gif.create(gif_url: result, category_id: category_id )
-    redirect_to admin_categories_path
   end
 
   def destroy
